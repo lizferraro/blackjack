0c044025c02d1e07c31c6056c93ff1dde1cb9e3a
Liz Ferraro <elizabeth.k.ferraro@gmail.com>
2022-07-13 16:01:18 -0400

    building out js and began css styling

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let firstCard = Math.floor(Math.random() * 12) + 2;
-let secondCard = Math.floor(Math.random() * 12) + 2;
-let sum = firstCard + secondCard;
+let firstCard = Math.floor(Math.random() * 12) + 2
+let secondCard = Math.floor(Math.random() * 12) + 2
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 console.log(firstCard)
 console.log(secondCard)
-console.log(sum)
+console.log("Sum: " + sum)
 
 if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } else if(sum === 21) {
-    console.log("Congratulations! You've got Blackjack! 🥳")
-} else if(sum > 21) {
-    console.log("You're out of the game! 😭")
+    message = "Congratulations! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game! 😭"
+    isAlive = false
 }
+
+console.log(message)

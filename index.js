8d4bc06df0d3ea1252c83f81858ed0f799c1633e
Liz Ferraro <elizabeth.k.ferraro@gmail.com>
2022-07-14 15:30:37 -0400

    store new card value and add to sum

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let firstCard = Math.floor(Math.random() * 12) + 2
 let secondCard = Math.floor(Math.random() * 12) + 2
-let thirdCard = Math.floor(Math.random() * 12) + 2
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -10,7 +10,14 @@ let cardsEl = document.getElementById("cards-el")
 let sumEl = document.getElementById("sum-el")
 
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + ", "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum < 21) {
         message = "Do you want to draw a new card?"
@@ -24,19 +31,10 @@ function startGame() {
     messageEl.textContent = message
 }
 
-function newCard() {
-    console.log("Drawing a new card from the deck")
-    cardsEl.textContent = "Cards " + firstCard + ", " + secondCard + ", " + thirdCard
-    let sum = firstCard + secondCard + thirdCard
-    sumEl.textContent = "Sum: " + sum
-    if (sum < 21) {
-        message = "Do you want to draw a new card?"
-    } else if(sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
+function drawCard() {
+    let newCard = Math.floor(Math.random() * 12) + 2
+    cardsEl.textContent += ", " + newCard
+    sum += newCard
+    cards.push(newCard)
+    renderGame()
 }
